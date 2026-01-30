2b6c128f53617d2bf45f85797dffae7a542ad50d
chusky <tommaso.testa@students.uniroma2.eu>
2026-01-30 20:04:49 +0100

    boh

---
+++ src/main/java/it/tommaso/uniroma2/App.java
@@ -0,0 +1,7 @@
+package it.tommaso.uniroma2;
+
+public class App {
+    public static void main(String... args){
+        Main.main(args);
+    }
+}
--- src/main/java/it/tommaso/uniroma2/Main.java
+++ src/main/java/it/tommaso/uniroma2/Main.java
@@ -4,10 +4,7 @@ package it.tommaso.uniroma2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
@@ -16,15 +13,14 @@ public class Main extends Application{
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
 
-        //Inizia esecuzione aprendo libreria personale.
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),400, 400);
+        stage.setScene(scene);
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("src/main/resources/finestra1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720); //sostituisci numeri fissi
-        primaryStage.setTitle("Progetto Murakami");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.show();
+
 
     }
 
