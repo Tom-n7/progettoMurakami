96642f5515a241a6b1bd96b1dc0c2bdf8881e5a4
pinkman <pinkman@linux.it>
2026-02-01 17:55:17 +0100

    sei dia 2

---
+++ src/main/java/it/tommaso/uniroma2/controller/ControllerGraficoBenvenuto.java
@@ -0,0 +1,27 @@
+package it.tommaso.uniroma2.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerGraficoBenvenuto extends Application implements ControllerGrafico {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/it/tommaso/uniroma2/controller/schermatabenvenuto"));
+        Scene scene = new Scene(fxmlLoader.load(),1280, 720);
+        stage.setScene(scene);
+
+        stage.show();
+
+
+    }
+
+
+    @Override
+    public void start() {
+        Application.launch();
+    }
+}
---
+++ src/main/java/it/tommaso/uniroma2/view/DashboardLibreriaPersonale.java
@@ -0,0 +1,13 @@
+package it.tommaso.uniroma2.view;
+
+import javafx.fxml.FXML;
+
+public class DashboardLibreriaPersonale {
+
+    public void prova1(){
+        System.out.println("prova riuscita!");
+    }
+
+
+
+}
